361a2846724f14cc1e6eef0565f1031fac5cac80
ICE MrThink15000 <a01287002@tec.mx>
2026-02-03 17:19:11 -0600

    Saved temp

--- src/arduino.cpp
+++ src/arduino.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <ArduinoSTL.h>
-// #include <vector>
+// #includex <vector>
 #include "BNO.h"
 #include "constants.h"
 #include "motor.h"
@@ -13,31 +13,39 @@
 // );
 Bno bno;
 
-Motors motors(
-    kMotor1Pwm, kMotor1In1, kMotor1In2,
-    kMotor2Pwm, kMotor2In1, kMotor2In2,
-    kMotor3Pwm, kMotor3In1, kMotor3In2
-);
+Motor motor1(kMotor1Pwm, kMotor1In1, kMotor1In2);
+Motor motor2(kMotor2Pwm, kMotor2In1, kMotor2In2);
+Motor motor3(kMotor3Pwm, kMotor3In1, kMotor3In2);
+
+// Motors motors(
+//     kMotor1Pwm, kMotor1In1, kMotor1In2,
+//     kMotor2Pwm, kMotor2In1, kMotor2In2,
+//     kMotor3Pwm, kMotor3In1, kMotor3In2
+// );
 
 void setup() {
     Serial.begin(9600);
-    motors.InitializeMotor();
+    motor1.InitializeMotor();
+    motor2.InitializeMotor();
+    motor3.InitializeMotor();
     bno.InitializeBNO();
+    Serial.println("Setup complete");
+    delay(5000); // Prevenir que el robot se suicide
+
 }
 
 void loop() {
-    float yaw = bno.GetBNOData();
-    Serial.print("Yaw: ");
-    Serial.println(yaw);
-
-    // motors.MoveRobot(0.15, FORWARD, UL);
-    // delay(1000);
-    // motors.StopMotor();
-    // delay(1000);
-
-    // motors.MoveRobot(0.15, BACKWARD, UL); 
-    // delay(1000);
-    // motors.StopMotor();
-    // delay(1000);
+    // float yaw = bno.GetBNOData();
+    // Serial.print("Yaw: ");
+    // Serial.println(yaw);
+    motor1.MoveMotor(30.0, FORWARD);
+    delay(1000);
+    motor1.StopMotor();
+    motor2.MoveMotor(30.0, FORWARD);
+    delay(1000);
+    motor2.StopMotor();
+    motor3.MoveMotor(30.0, FORWARD);
+    delay(1000);
+    motor3.StopMotor(); // This one moves counterclockwise
     
 }
