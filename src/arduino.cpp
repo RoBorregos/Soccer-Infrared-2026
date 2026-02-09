ea8b11c15595bc40f3b66231d8991de1bdd759a4
ICE MrThink15000 <a01287002@tec.mx>
2026-02-09 15:20:24 -0600

    Added debug functions

--- src/arduino.cpp
+++ src/arduino.cpp
@@ -5,27 +5,26 @@
 #include "constants.h"
 #include "motors.h"
 
+// DEBUG 
+#include "motorsTest.cpp"
+
 Motors motors;
 Bno bno;
+
+Debug debug;
 
 void setup() {
     Serial.begin(9600);
     motors.begin();
     bno.begin();
     Serial.println("Setup complete");
-    // delay(5000);
+    delay(5000);
 }
 
 void loop() {
-    int base_speed = 65;
-    motors.move(0, base_speed);
-    delay(650);
-    motors.move(90, base_speed);
-    delay(650);
-    motors.move(180, base_speed);
-    delay(650);
-    motors.move(270, base_speed);
-    delay(650);
-    motors.stop();
+    debug.motorSquare();
+    delay(1000);
+    debug.motorMoveIndividualMotors();
+
     for(;;);
 }
