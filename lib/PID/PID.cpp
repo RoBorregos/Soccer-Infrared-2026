cef2302ee414185df36251a1b8f890b9d08f71ff
ICE MrThink15000 <a01287002@tec.mx>
2026-02-11 15:17:14 -0600

    Added old PID

--- lib/PID/PID.cpp
+++ lib/PID/PID.cpp
@@ -1,68 +1,91 @@
 #include "PID.h"
 
 void PID::reset(PIDParameters& pid) {
-    pid.integral_error = 0;
-    pid.previous_error = 0;
-    pid.output = 0;
-    pid.error = 0;
-    pid.target = 0;
+    pid.integral_error = 0.0;
+    pid.previous_error = 0.0;
+    pid.output = 0.0;
+    pid.error = 0.0;
+    pid.target = 0.0;
     pid.last_iteration_ms = 0;
     pid.first_run = true;
 }
 
 double PID::calculate(PIDParameters& pid) {
     unsigned long long current_time_ms = millis();
-    
-    // FROM AIZER PID IMPLEMENTATION
+
+    pid.error = pid.target - pid.current_value;
+    // Serial.print(" | C: ");
+    // Serial.print(pid.current_value);
+    // Serial.print(" | E: ");
+    Serial.print(pid.error);
     if (pid.first_run) {
         pid.last_iteration_ms = current_time_ms;
         pid.previous_error = pid.error;
         pid.integral_error = 0.0;
         pid.first_run = false;
 
-        const double pTerm = pid.kp * pid.error;
-        double initial_output = pTerm;
-
-        if (pid.min_output > 0.0 && fabs(initial_output) < pid.min_output) {
-            initial_output = 0.0;
-        }
+        double pTerm = pid.kp * pid.error;
+        double output = pTerm;
 
         if (pid.max_output > 0.0) {
-            initial_output = constrain(
-                initial_output,
-                -pid.max_output,
-                pid.max_output
-            );
+            output = constrain(output, -pid.max_output, pid.max_output);
+        }
+        if (pid.min_output > 0.0 && fabs(output) > 0.0 && fabs(output) < pid.min_output) {
+            output = (output > 0.0) ? pid.min_output : -pid.min_output;
         }
 
-        pid.output = initial_output;
+        Serial.print("EXECUTED ONCE");
+        pid.output = output;
         return pid.output;
     }
 
-    double dt = (current_time_ms - pid.last_iteration_ms) / 1000.0; 
-    double previous_integral = pid.integral_error;
-    pid.error = pid.target - pid.current_value;
-
-
-    if (dt <= 0.0) return pid.output; 
-    if (abs(pid.error) < pid.error_threshold) {
-        return 0.0; 
+    double dt = (current_time_ms - pid.last_iteration_ms) / 1000.0;
+    Serial.print("dt: ");
+    Serial.println(dt, 5);
+    if (dt <= 0.0) {
+        return pid.output;
     }
 
-    pid.integral_error += pid.error * dt;
+    if (fabs(pid.error) < pid.error_threshold) {
+        pid.output = 0.0;
+        return pid.output;
+    }
+    double pTerm = pid.kp * pid.error;
+    double dTerm = pid.kd * (pid.error - pid.previous_error) / dt;
+    // Tentative integral for anti-windup
+    double newIntegral = pid.integral_error + pid.error * dt;
+    double iTerm = pid.ki * newIntegral;
 
-    double derivative_error = (pid.error - pid.previous_error) / dt;
-    double output = (pid.kp * pid.error) + (pid.ki * pid.integral_error) + (pid.kd * derivative_error);
+    Serial.print("P: ");
+    Serial.print(pTerm, 5);
+    Serial.print(" | I: ");
+    Serial.print(iTerm, 5);
+    Serial.print(" | D: ");
+    Serial.print(dTerm, 5);
+    Serial.print(" | O: ");
+    Serial.println(pTerm + iTerm + dTerm, 5);
 
-    if(output > pid.max_output && output < 0) {
-        pid.current_value = constrain(pid.current_value, pid.min_output, pid.max_output);
-        pid.integral_error = previous_integral;
+    double output = pTerm + iTerm + dTerm;
 
-    } else if (output < pid.min_output && output > 0) {
-        pid.current_value = constrain(pid.current_value, -pid.max_output, -pid.min_output);
-        pid.integral_error = previous_integral;
+    // Anti-windup & clamping
+    if (pid.max_output > 0.0) {
+        if (output > pid.max_output) {
+            output = pid.max_output;
+            newIntegral = pid.integral_error;
+        } else if (output < -pid.max_output) {
+            output = -pid.max_output;
+            newIntegral = pid.integral_error;
+        }
     }
 
+    // Ensure at least min_output magnitude if non-zero, otherwise freeze integral
+    if (pid.min_output > 0.0 && fabs(output) > 0.0 && fabs(output) < pid.min_output) {
+        output = (output > 0.0) ? pid.min_output : -pid.min_output;
+        newIntegral = pid.integral_error;
+    }
+
+    // Commit state
+    pid.integral_error = newIntegral;
     pid.previous_error = pid.error;
     pid.last_iteration_ms = current_time_ms;
     pid.output = output;
