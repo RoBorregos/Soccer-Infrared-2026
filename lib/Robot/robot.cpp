a14f81129f1635cf536402a4a11076517e2a8140
ICE MrThink15000 <a01287002@tec.mx>
2026-03-31 14:18:31 -0600

    Init I2C (Wire) in Robot::begin before IMU init

--- lib/Robot/robot.cpp
+++ lib/Robot/robot.cpp
@@ -7,6 +7,9 @@ Robot::Robot() : imu(&IMU, IMU_ADDRESS) {
 void Robot::begin() {
     motors.begin();
     // bno.begin();
+    // Initialize I2C for IMU use (matches tests)
+    Wire.begin();
+    Wire.setClock(400000);
     int err = imu.begin(calib);
     if (err != 0) {
         Serial.print("IMU begin failed: ");
