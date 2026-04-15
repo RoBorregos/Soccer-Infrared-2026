773a5085f766b8512eef447a5e9dbb03c96123a3
TEC-Andres <a01287002@tec.mx>
2026-04-14 20:43:35 -0600

    Updated robot class to match dev2

--- lib/Robot/robot.cpp
+++ lib/Robot/robot.cpp
@@ -1,6 +1,18 @@
 #include "robot.h"
 
+Robot::Robot() {
+    currentTime = millis();
+}
+
 void Robot::begin() {
+    // Motors
     motors.begin();
+
+    // BNO085
+    Wire.begin();
+    Wire.setClock(400000);
     bno.begin();
+
+    // IR Ring
+    irring.init(&currentTime);
 }
